100acfb29aaa826255c53d1cb3727388ae48f608
林庭楷 <hellolinpeter@gmail.com>
2025-02-11 23:25:39 +0800

    change: "main.go"

--- main.go
+++ main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"flag"
+	"github.com/peterouob/golang_template/configs"
 	"github.com/peterouob/golang_template/server"
 	"github.com/peterouob/golang_template/tools"
 )
 
 var (
-	port = flag.Int("port", 8080, "grpc service port")
+	port = flag.Int("port", 8082, "grpc service port")
 )
 
 func init() {
 	tools.InitLogger()
+	configs.InitViper()
 }
 
 func main() {
 	flag.Parse()
-	go func() {
-		server.InitGrpcServer(*port)
-	}()
+	//server.InitGrpcServer(*port)
+	go server.InitLoginServer()
+	go server.InitTokenServer()
 
 	server.GrpcGatewayServer(*port)
 }
