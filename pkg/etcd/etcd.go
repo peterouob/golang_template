21f7b925dec0235c2950cfc5d0b0affbcc50df2b
林庭楷 <hellolinpeter@gmail.com>
2025-02-12 13:14:28 +0800

    change: "rebuild etcd pkg"

---
+++ pkg/etcd/etcd.go
@@ -0,0 +1,52 @@
+package etcdregister
+
+import (
+	"fmt"
+	etcdservice "github.com/peterouob/golang_template/pkg/etcd/server"
+	"github.com/peterouob/golang_template/tools"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+type EtcdRegister struct {
+	client  *etcdservice.EtcdService
+	leaseId clientv3.LeaseID
+	heart   int64
+}
+
+func NewEtcdRegister(endpoints []string, heart int64) *EtcdRegister {
+	return &EtcdRegister{
+		client: etcdservice.RegisterETCD(endpoints, heart),
+		heart:  heart,
+	}
+}
+
+func (e *EtcdRegister) Register(serviceName, addr string) {
+	e.leaseId = e.client.Register(serviceName, addr, 0)
+	tools.Log(fmt.Sprintf("Registered service %s at %s", serviceName, addr))
+	go func() {
+		for {
+			e.client.Register(serviceName, addr, e.leaseId)
+			time.Sleep(time.Duration(e.heart)*time.Second - 100*time.Millisecond)
+		}
+	}()
+}
+
+func (e *EtcdRegister) UnRegister(serviceName, addr string) {
+	e.client.UnRegister(serviceName, addr)
+	tools.Log(fmt.Sprintf("unregiter service: %s from etcd, addr: %s", serviceName, addr))
+}
+
+func (e *EtcdRegister) ListenExit(serviceName, addr string) {
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		sig := <-c
+		tools.Log(fmt.Sprintf("receive a signal %s", sig.String()))
+		e.client.UnRegister(serviceName, addr)
+		os.Exit(0)
+	}()
+}
--- test/etcd/etcd_test.go
+++ test/etcd/etcd_test.go
@@ -2,11 +2,13 @@ package etcd
 
 import (
 	"context"
+	"fmt"
 	"github.com/peterouob/golang_template/api/protobuf"
 	"github.com/peterouob/golang_template/configs"
 	grpcclient "github.com/peterouob/golang_template/pkg/grpc_service/client"
 	"github.com/peterouob/golang_template/tools"
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/metadata"
 	"testing"
 )
 
@@ -19,11 +21,37 @@ func TestEchoEtcd(t *testing.T) {
 	cfg.SetPoolSize(4)
 	cfg.SetEndPoints([]string{"127.0.0.1:2379"})
 	cfg.SetServiceName("echo_service")
-	client, pool, err := grpcclient.GetGRPCClient(cfg, "echo_service")
+	client, err := grpcclient.GetGRPCClient(cfg, "echo_service")
 	assert.NotNil(t, client)
-	assert.NotNil(t, pool)
 	assert.NoError(t, err)
 	resp, err := client.(protobuf.EchoClient).Echo(context.Background(), &protobuf.EchoRequest{Name: "Hello"})
 	assert.NoError(t, err)
 	assert.NotNil(t, resp)
+}
+
+func TestLoginServer(t *testing.T) {
+	cfg := &configs.EtcdGrpcCfg{}
+	cfg.SetPoolSize(4)
+	cfg.SetEndPoints([]string{"127.0.0.1:2379"})
+	cfg.SetServiceName("login_service")
+	client, err := grpcclient.GetGRPCClient(cfg, "login_service")
+	assert.NoError(t, err)
+	r, err := client.(protobuf.UserClient).LoginUser(context.Background(), &protobuf.LoginUserRequest{
+		Email:    "admin",
+		Password: "admin",
+	})
+	assert.NotNil(t, r)
+	testToken(t, r.AccessToken)
+}
+
+func testToken(t *testing.T, token string) {
+	cfg := &configs.EtcdGrpcCfg{}
+	cfg.SetPoolSize(4)
+	cfg.SetEndPoints([]string{"127.0.0.1:2379"})
+	cfg.SetServiceName("tokentest_service")
+	client, err := grpcclient.GetGRPCClient(cfg, "tokentest_service")
+	assert.NoError(t, err)
+	ctx := metadata.AppendToOutgoingContext(context.Background(), "authorization", fmt.Sprintf("Bearer %s", token))
+	r, err := client.(protobuf.UserClient).TokenTest(ctx, &protobuf.TokenTestRequest{})
+	t.Logf("%v", r)
 }
